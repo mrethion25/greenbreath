e7c6b29b4d06fcc947a2e0bc87436ee4d01d4fc6
mrethion25 <worldof2512@gmail.com>
2025-11-26 19:48:14 +0530

    Update Faq.jsx

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -11,7 +11,7 @@ const Faq = () => {
         <div className="container relative z-2 py-28">
           <div>
             <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
-              Curiosity didn't kill the cat, it gave it answers.
+              FAQs about greenbreath
             </h3>
             <p className="body-1 max-lg:max-w-sm">
               You've got questions, we've got answers.
