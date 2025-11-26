b22999c697089456191d0bc74a021ceaccf9bc73
mrethion25 <worldof2512@gmail.com>
2025-11-26 19:54:00 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
             <div className="caption small-2 uppercase text-p3 ">
-              Video Editing
+              Easy to install
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-2 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              Amazingly Simple
+              Highly efficient 
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
-              We designed XORA AI to be an eassy to use, quick to learn, and
-              surprisingly powerful.
+              breathing is an essential part of life, each and every
+              living organism deserves clean air for cheap!
             </p>
             <LinkScroll to="features" offset={-100} spy smooth>
               <Button icon="/images/zap.svg">Try it now</Button>
