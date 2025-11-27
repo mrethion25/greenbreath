2dfc957fad0dcdf376812340f7302c51f5215ad0
mrethion25 <worldof2512@gmail.com>
2025-11-27 19:30:08 +0530

    Update Pricing.jsx

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -116,7 +116,7 @@ const Pricing = () => {
                       />
                     </div>
                     <div className="small-1 relative top-3 ml-1 uppercase">
-                      / mo
+                      / quantity
                     </div>
                   </div>
                 </div>
