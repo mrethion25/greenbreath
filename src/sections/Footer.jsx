4306ca109fdf6775d2e4102cc205c27ef8387b86
mrethion25 <worldof2512@gmail.com>
2025-11-28 15:51:36 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
       <div className="container py-10">
         <div className="flex w-full max-md:flex-col">
           <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
-            <p className="opacity-70">Copyright, aarav jha AKA arima</p>
+            <p className="opacity-70">made with ❤️ by aarav jha AKA arima</p>
           </div>
           <div className="flex items-center justify-center sm:ml-auto">
             <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
-              Privacy Policy
+              FOR PROJECT PURPOSE ONLY 
             </p>
             <p className="text-p5 transition-all duration-500 hover:text-p1">
-              Terms of Use
+              ALL RIGHTS RESERVED
             </p>
           </div>
 
